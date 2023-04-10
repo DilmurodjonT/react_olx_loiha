a3222affb69ed0d304c7c4e70c7c7e1e081d603e
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-04-10 17:43:14 +0500

    add all products home

---
+++ src/components/all-products-home/AllProductsHome.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import c from "./AllProductsHome.module.scss";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const AllProductsHome = () => {
+  const [AllProductsData, setAllProductsData] = useState([]);
+
+  useEffect(() => {
+    axios("https://seal-app-42lge.ondigitalocean.app/category/category-reel")
+      .then((response) => setAllProductsData(response.data))
+      .catch((err) => console.error(err));
+  }, []);
+  return (
+    <div>
+      {AllProductsData.splice(0, AllProductsData.length - 1).map(
+        (categoryItem) => (
+          <div>
+            <h2>{categoryItem.categoryName_uz} ===========</h2>
+            <Swiper
+              slidesPerView={7}
+              spaceBetween={30}
+              navigation={true}
+              pagination={{
+                clickable: true,
+              }}
+              className="mySwiper"
+              modules={Navigation}
+            >
+              {categoryItem.allRefinedProducts.map((product) => (
+                <SwiperSlide>
+                  <div className={c.product__card}>
+                    <img src={product.productImages[0]} alt="" />
+                    <h3>{product.productName_uz}</h3>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default AllProductsHome;
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AllCategories from "../../components/all-categories/AllCategories";
+import AllProductsHome from "../../components/all-products-home/AllProductsHome";
 import { Container } from "../../utils/Components";
 const Home = () => {
   return (
     <div>
       <Container>
         <AllCategories />
+        <AllProductsHome />
       </Container>
     </div>
   );
