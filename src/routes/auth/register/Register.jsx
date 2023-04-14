852369a66e859baab549a7aa3cac7552d2f6013b
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-04-14 23:20:52 +0500

    end style register section

--- src/routes/auth/register/Register.jsx
+++ src/routes/auth/register/Register.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import c from "./Register.module.scss";
+import { useTranslation } from "react-i18next";
 
 const Register = () => {
+  const { t } = useTranslation();
   const [userdata, setUserdata] = useState({
     name: "",
     email: "",
@@ -22,14 +24,14 @@ const Register = () => {
         <input
           required
           type="text"
-          placeholder="Your name"
+          placeholder={t("your name")}
           value={userdata.name}
           onChange={(e) => setUserdata({ ...userdata, name: e.target.value })}
         />
         <input
           required
           type="email"
-          placeholder="Your email"
+          placeholder={t("your email")}
           value={userdata.email}
           onChange={(e) => setUserdata({ ...userdata, email: e.target.value })}
         />
@@ -37,7 +39,7 @@ const Register = () => {
           required
           minLength={8}
           type="password"
-          placeholder="Your password"
+          placeholder={t("your password")}
           value={userdata.password}
           onChange={(e) =>
             setUserdata({ ...userdata, password: e.target.value })
@@ -46,11 +48,11 @@ const Register = () => {
         <input
           required
           type="url"
-          placeholder="Your avatar"
+          placeholder={t("your avatar")}
           value={userdata.avatar}
           onChange={(e) => setUserdata({ ...userdata, avatar: e.target.value })}
         />
-        <button type="submit">Register</button>
+        <button type="submit">{t("register")}</button>
       </form>
     </div>
   );
